e5db707d3d7405846a7ca1176a495606bbe6f5c7
GlobalSafety <seu-email@globalsafety.ao>
2025-12-19 15:33:43 +0100

    fix: eslint config commonjs com ignores para prisma

---
+++ eslint.config.js
@@ -0,0 +1,1 @@
+module.exports = [{ignores:['**/node_modules/**','**/.next/**','**/app/generated/prisma/**','**/node_modules/.prisma/**','**/node_modules/@prisma/client/**','**/*.prisma/**','**/prisma/**']}]; 
